8866547bd5f8bbe66b4543c1326f4c22f57efb7c
Hemalatha1428 <24nn1a4277@gmail.com>
2026-07-10 16:19:25 +0530

    Add files via upload

---
+++ mini_chatbot/app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from openai import OpenAI
+
+# =====================================================================
+# Page Configuration
+st.set_page_config(
+    page_title="Mini ChatGPT - Mistral",
+    page_icon="🦁",
+    layout="centered"
+)
+
+# =====================================================================
+st.title("🦁 Mini ChatGPT {Mistral AI}")
+
+api_key = "6dhNGJZP0KrsDYe0TsL1P5OkXlza410a"
+
+# =====================================================================
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "system", "content": "You are a helpful AI assistant."}
+    ]
+
+# =====================================================================
+# Show previous messages
+for message in st.session_state.messages:
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+# =====================================================================
+# Chat input
+prompt = st.chat_input("Type your message...")
+
+if prompt:
+    if not api_key:
+        st.error("Please enter API key.")
+        st.stop()
+
+    client = OpenAI(
+        api_key=api_key,
+        base_url="https://api.mistral.ai/v1"
+    )
+
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
+
+    # Display assistant message
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = client.chat.completions.create(
+                model="mistral-small-latest",
+                messages=st.session_state.messages
+            )
+
+            reply = response.choices[0].message.content
+            st.markdown(reply)
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": reply}
+    )
+#=================================================================================
+#slide bar
+with st.sidebar:
+    st.header("Options")
+    if st.button("Clear Chat"):
+        st.session_state.messages = [{"role": "system",
+        "content": "you are a helpful AI"}]
+        st.rerun()
+    st.markdown("---")
+    st.write("**Model:** mistral-small-latest")
